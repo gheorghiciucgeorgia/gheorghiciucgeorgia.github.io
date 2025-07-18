bdd017d339346f693cfec8d6b36300ba2f59328b
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-07-18 22:06:12 +0300

    first commit

---
+++ portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import MobileMenu from './MobileMenu.tsx'
+import Navbar from './Navbar.tsx'
+
+const Header  = () =>{
+return(
+<>
+  <MobileMenu />
+  <Navbar />
+</>
+)};
+export default Header;
---
+++ portfolio/src/sections/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1 className="hero__title">Welcome to My Portfolio</h1>
+      <p className="hero__description">I am a passionate developer.</p>
+    </section>
+  );
+};
+
+export default Hero;
