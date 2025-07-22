71ee894efe1b28b0ccf01ae96bff46fce5789fe2
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-07-22 16:54:00 +0300

    header component updated

--- portfolio/src/components/Header/Header.tsx
+++ portfolio/src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
-// import MobileMenu from './MobileMenu.tsx'
+import MobileMenu from './MobileMenu.tsx'
 import Navbar from './Navbar.tsx'
 
-const Header  = () =>{
-return(
-<>
-  {/* <MobileMenu /> Will uncomment when needed */}
-  <Navbar />
-</>
-)};
+const Header = () => {
+  return (
+    <>
+      {/* MobileMenu doar pe mobil */}
+      <div className="block lg:hidden">
+        <MobileMenu />
+      </div>
+
+      {/* Navbar doar pe desktop */}
+      <div className="hidden lg:block">
+        <Navbar />
+      </div>
+    </>
+  )
+};
 export default Header;
