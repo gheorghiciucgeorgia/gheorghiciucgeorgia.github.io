4e4830c744f3aa119a6ae1f033897bbd3124dd8f
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-08-14 23:42:47 +0300

    app update

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 // import Header from './components/Header/Header.tsx';
 // import Button from './components/Button/Button.tsx';
 // import ProjectCard from './components/Card/ProjectCard.tsx';
-import SkillCard from './components/Card/SkillCard.tsx';
+// import SkillCard from './components/Card/SkillCard.tsx';
+import Footer from './components/Footer/Footer.tsx';
 
 function App() {
 
@@ -16,9 +17,13 @@ function App() {
       {/* <Button name='cv-btn' text='View CV' colorText='var(--secondary-color)' colorFrame='var(--secondary-color)' />
       <Button name='card-btn' text='View More' />
       <Button name='contact-btn' text='Contact Me' /> */}
+
+
       {/* <ProjectCard colorFrame="var(--accent-color)" className="w-[400px]" title="Lorem Ipsum" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat." imageUrl="/assets/placeholder-img-card.png" /> */}
 
-      <SkillCard icon="fa-brands fa-html5" title="HTML5" colorFrame="var(--accent-color)" colorText="var(--primary-color)" width={400} />
+      {/* <SkillCard icon="fa-brands fa-html5" title="HTML5" colorFrame="var(--accent-color)" colorText="var(--primary-color)" width={400} /> */}
+
+      <Footer></Footer>
     </>
   )
 }
