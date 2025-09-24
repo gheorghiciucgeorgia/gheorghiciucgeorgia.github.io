5d80363fc6c0813c737885065e45196195d323e7
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-09-25 00:10:47 +0300

    Header bg color changed

--- portfolio/src/components/Header/Header.tsx
+++ portfolio/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className='w-full bg-[var(--primary-color)]'>
+    <div className='w-full'>
       <div className='menu'>
         {/* MobileMenu doar pe mobil */}
         <div className="block lg:hidden">
@@ -12,7 +12,7 @@ const Header = () => {
         </div>
 
         {/* Navbar doar pe desktop */}
-        <div className="hidden lg:block">
+        <div className="hidden bg-[var(--primary-color)] lg:block">
           <Navbar />
         </div>
       </div>
