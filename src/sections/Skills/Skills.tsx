0a574625c709b21da20b3b89747d52aa3bd0374c
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-10-02 21:57:36 +0300

    Skills - section added desktop version

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,27 +1,56 @@
+import SkillCard from '../../components/Card/SkillCard';
 import './Skills.css';
 
 const Skills = () => {
-    const colorFrame = "var(--accent-color)";
-    const pathFrame = () => {
-        return (
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 500 100"
-                className="w-full h-auto"
-            >
-                <path fill="none" stroke={colorFrame} strokeWidth={11} strokeMiterlimit={10} d="M0 100.6C83.3 66.9 166.7 33.1 250-.6" />
-                <path fill="none" stroke={colorFrame} strokeWidth={11} strokeMiterlimit={10} d="M500 100.6C416.7 66.9 333.3 33.1 250-.6" />
-            </svg>
-        );
-    }
     return (
-        <div className="scroll-container w-full h-auto bg-[var(--primary-color)] py-5 overflow-x-auto overflow-y-hidden">
-            <div className="w-full h-auto flex flex-row items-center min-w-[max-content]">
-                {[...Array(10)].map((_, i) => (
-                    <div className="flex-shrink-0" key={i}>{pathFrame()}</div>
-                ))}
+        <div className="flex flex-col items-center justify-center w-full h-auto bg-[var(--primary-color)] py-5 border-t-2 border-t-[var(--accent-color)] border-b-2 border-b-[var(--accent-color)]">
+            <div className='w-full h-auto flex flex-col justify-between gap-4'>
+                <div className='title-languages'>
+                    <h2 className='font-(family-name:--heading-font) text-[var(--accent-color)] text-center text-[24pt]'><i className="cybg-star-21 cybg mr-2"></i>Languages<i className="cybg-star-21 cybg ml-2"></i></h2>
+                </div>
+                <div className='w-full h-auto flex flex-row justify-evenly items-center border-b-2 border-b-[var(--accent-color)] pb-7'>
+                    <SkillCard icon="fa-brands fa-html5" title="HTML5" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="fa-brands fa-css3" title="CSS3" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="fa-brands fa-js" title="JS (ES6+)" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bi bi-typescript" title="TypeScript" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="fa-brands fa-php" title="PHP" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                </div>
+                <div className='title-frameworks-libraries'>
+                    <h2 className='font-(family-name:--heading-font) text-[var(--accent-color)] text-center text-[24pt]'><i className="cybg-star-15 cybg mr-2"></i>Frameworks & Libraries<i className="cybg-star-15 cybg ml-2"></i></h2>
+                </div>
+                <div className='w-full h-auto flex flex-row justify-evenly items-center border-b-2 border-b-[var(--accent-color)] pb-7'>
+                    <SkillCard icon="fa-brands fa-react" title="React.js" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="fa-brands fa-angular" title="Angular" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bxl bx-jquery" title="jQuery" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bxl  bx-gsap" title="GSAP" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                </div>
+                <div className='title-styling'>
+                    <h2 className='font-(family-name:--heading-font) text-[var(--accent-color)] text-center text-[24pt]'><i className="cybg-flower-03 cybg mr-2"></i>Styling & UI<i className="cybg-flower-03 cybg ml-2"></i></h2>
+                </div>
+                <div className='w-full h-auto flex flex-row justify-evenly items-center border-b-2 border-b-[var(--accent-color)] pb-7'>
+                    <SkillCard icon="fa-brands fa-sass" title="SASS" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="ri-tailwind-css-fill" title="Tailwind" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="fa-brands fa-bootstrap" title="Bootstrap" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bi bi-filetype-psd" title="Phtoshop" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bxl bx-adobe" title="Illustrator" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                </div>
+                <div className='title-styling'>
+                    <h2 className='font-(family-name:--heading-font) text-[var(--accent-color)] text-center text-[24pt]'><i className="cybg-flower-10 cybg mr-2"></i>Tools & Workflow<i className="cybg-flower-10 cybg ml-2"></i></h2>
+                </div>
+                <div className='w-full h-auto flex flex-row justify-evenly items-center border-b-2 border-b-[var(--accent-color)] pb-7'>
+                    <SkillCard icon="fa-brands fa-git-alt" title="Git" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="fa-solid fa-cube" title="Webpack" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bi bi-globe-americas" title="REST API" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bi bi-filetype-json" title="JSON" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                    <SkillCard icon="bxl  bx-vite-js" title="Vite" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                </div>
+                <div className='title-styling'>
+                    <h2 className='font-(family-name:--heading-font) text-[var(--accent-color)] text-center text-[24pt]'><i className="cybg-flower-16 cybg mr-2"></i>Content Management System<i className="cybg-flower-16 cybg ml-2"></i></h2>
+                </div>
+                <div className='w-full h-auto flex flex-row justify-evenly items-center'>
+                    <SkillCard icon="fa-brands fa-drupal" title="Drupal FE" colorFrame="var(--accent-color)" colorText="var(--secondary-color)" width={180} />
+                </div>
             </div>
-
         </div>
     );
 }
