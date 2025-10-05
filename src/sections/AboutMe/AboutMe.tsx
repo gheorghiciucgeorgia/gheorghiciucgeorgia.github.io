de7a8ce0553d5816295a3520a8b9a8db953f99e7
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-10-05 22:04:47 +0300

    AboutMe - section optimization

--- src/sections/AboutMe/AboutMe.tsx
+++ src/sections/AboutMe/AboutMe.tsx
@@ -68,7 +68,7 @@ const AboutMe = () => {
     }, []);
 
     return (
-        <div ref={aboutMeRef} className='aboutMe-section w-full bg-[var(--secondary-color)] flex flex-col lg:flex-row justify-between items-center py-[4vw] lg:py-[2vw] px-[7vw] relative before:top-[-6px] md:before:top-[-18px] xl:before:top-[-35px]' id="About">
+        <div ref={aboutMeRef} className='aboutMe-section w-full bg-[var(--secondary-color)] flex flex-col lg:flex-row justify-between items-center py-[4vw] lg:py-[2.5vw] px-[7vw] relative before:top-[-6px] md:before:top-[-18px] xl:before:top-[-35px]' id="About">
             <div ref={sphereRef} className="wrapper mb-[20px] lg:mb-0 lg:w-[45%]">
                 <div className="sphere w-[100px] h-[100px] lg:w-[200px] lg:h-[200px]">
                     <div className="equator rotate-x-[90deg] w-[100px] h-[100px] lg:w-[200px] lg:h-[200px]"></div>
@@ -79,7 +79,7 @@ const AboutMe = () => {
             <div className='w-full lg:w-[45%] text-[var(--accent-color)]'>
                 <p ref={textRef} style={{ fontFamily: 'var(--body-font)' }} className='border-l-5 border-l-solid p-3 text-justify text-[16px] lg:text-[18px] leading-1.4 mb-[20px] lg:mb-0 lg:text-start'>A little bit about me: I am adaptable, detail-oriented, and always eager to learn. I approach every project with curiosity and a passion for creating meaningful results. Passionate about creating engaging and intuitive web experiences, I strive to make every project meaningful.</p>
             </div>
-            <div ref={bottomRef} className='aboutMe-bottom absolute bottom-[3px] italic text-[10px] lg:text-[16px] left-[8px] line-height-[110%] text-[var(--accent-color)] opacity-80'>
+            <div ref={bottomRef} className='aboutMe-bottom absolute bottom-[6px] italic text-[10px] lg:text-[16px] left-[8px] line-height-[110%] text-[var(--accent-color)] opacity-80'>
                 <i className="ri-error-warning-line mr-1 text-[12px] lg:text-[18px]"></i>SYNCING EXPERIENCE...
             </div>
         </div>
